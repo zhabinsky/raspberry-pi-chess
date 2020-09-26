f8742d8bb8b64a631ebc02e979f104784a0a1a08
Vlad Zhabinsky <vladislavzhabinsky@gmail.com>
2020-09-27 00:33:54 +0300

    No message specified

--- src/index.js
+++ src/index.js
@@ -73,7 +73,7 @@ const motorNextStates = steps => {
       ? sequence
       : [...sequence].reverse ()) {
       await device.writeStates (states);
-      await wait (4);
+      await wait (2);
     }
 
     await loop (device, direction, step + 1);
