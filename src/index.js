d25f952aa46e561edcf55a8505063ea3b830a4bc
Vlad Zhabinsky <vladislavzhabinsky@gmail.com>
2020-09-26 18:25:49 +0300

    test

--- src/index.js
+++ src/index.js
@@ -41,3 +41,15 @@ const cicle = () =>
 cicle ();
 
 console.log ('meow');
+
+const Gpio = require ('onoff').Gpio; // Gpio class
+const led = new Gpio (4, 'out'); // Export GPIO17 as an output
+
+// Toggle the state of the LED connected to GPIO17 every 200ms
+const iv = setInterval (_ => led.writeSync (led.readSync () ^ 1), 200);
+
+// Stop blinking the LED after 5 seconds
+setTimeout (_ => {
+  clearInterval (iv); // Stop blinking
+  led.unexport (); // Unexport GPIO and free resources
+}, 5000);
