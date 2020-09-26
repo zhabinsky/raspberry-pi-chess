faf155aa12f2c9e5ba5cee139b891f6831e4e320
Vlad Zhabinsky <vladislavzhabinsky@gmail.com>
2020-09-26 20:22:52 +0300

    No message specified

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ const sequence = [
   [0, 0, 0, 1],
 ];
 
-const gpioMotor = gpioInterface (13, 12, 6, 5);
+const gpioMotor = gpioInterface (16, 18, 22, 7);
 
 // setInterval (() => gpioLed.switchAll (), 1000);
 
