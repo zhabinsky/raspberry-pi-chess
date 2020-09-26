95426b3d2df646d304b06edb7ee6af78123ccb6d
Vlad Zhabinsky <vladislavzhabinsky@gmail.com>
2020-09-26 21:22:13 +0300

    No message specified

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ const motorNextStates = async () => {
   for (const states of sequence) {
     await gpioMotor.writeStates (states);
     console.log ('_____');
-    await wait (200);
+    await wait (20);
   }
   // for (const states of [...sequence].reverse ()) {
   //   await gpioMotor.writeStates (states);
