8530bbc24982f35ce020cf4756c1a1c542bc7b21
Vlad Zhabinsky <vladislavzhabinsky@gmail.com>
2020-09-26 23:39:39 +0300

    No message specified

--- src/index.js
+++ src/index.js
@@ -74,7 +74,7 @@ const motorNextStates = steps => {
 
     for (const states of sequence) {
       await device.writeStates (states);
-      await wait (1);
+      await wait (1.5);
     }
 
     loop (device, step + 1);
