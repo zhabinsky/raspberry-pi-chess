4d7cb474499940db60b009ab03960af851b5276a
Vlad Zhabinsky <vladislavzhabinsky@gmail.com>
2020-09-26 18:31:36 +0300

    tst

--- src/index.js
+++ src/index.js
@@ -46,10 +46,15 @@ const Gpio = require ('onoff').Gpio; // Gpio class
 const led = new Gpio (4, 'out'); // Export GPIO17 as an output
 
 // Toggle the state of the LED connected to GPIO17 every 200ms
-const iv = setInterval (_ => led.writeSync (led.readSync () ^ 1), 200);
+const iv = setInterval (_ => {
+  const value = led.readSync () ^ 1;
+
+  console.log (value);
+  led.writeSync (value);
+}, 200);
 
 // Stop blinking the LED after 5 seconds
 setTimeout (_ => {
   clearInterval (iv); // Stop blinking
   led.unexport (); // Unexport GPIO and free resources
-}, 5000);
+}, 999999999999);
