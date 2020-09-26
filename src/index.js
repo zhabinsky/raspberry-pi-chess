4d8379765942b37ded2ac10162b0a96de435ea7e
Vlad Zhabinsky <vladislavzhabinsky@gmail.com>
2020-09-27 00:54:22 +0300

    No message specified

--- src/index.js
+++ src/index.js
@@ -83,11 +83,14 @@ const motorNextStates = steps => {
 };
 
 (async () => {
-  const turnAround = motorNextStates (8);
+  const turnAround = motorNextStates (5);
   for (let i = 0; i < 70; i++) {
     await turnAround (gpioMotor2, 1);
     await turnAround (gpioMotor2, -1);
+    await turnAround (gpioMotor2, 1);
     await turnAround (gpioMotor2, -1);
+    await turnAround (gpioMotor2, -1);
+    await turnAround (gpioMotor2, 1);
     await turnAround (gpioMotor2, -1);
     await turnAround (gpioMotor2, 1);
     await turnAround (gpioMotor2, 1);
